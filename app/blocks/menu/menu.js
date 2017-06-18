cd7838923bec44482308dc7bdfddb252892b909c
alege <aleggge@gmail.com>
2017-06-18 21:54:12 +0300

    header2, footer2 final

--- app/blocks/menu/menu.js
+++ app/blocks/menu/menu.js
@@ -1,8 +1,12 @@
 import $ from 'jquery';
 
-export default () => { //eslint-disable-next-line
-  $('.burger').click(function () {
-    $(this).toggleClass('burger_active');
-    $('.menu').toggleClass('menu_active');
+export default () => {
+//eslint-disable-next-line
+  $('.header__burger').click(function () {
+    $('.menu').addClass('menu_active');
+  });
+  //eslint-disable-next-line
+  $('.burger_close').click(function () {
+    $('.menu').removeClass('menu_active');
   });
 };
