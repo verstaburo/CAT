f4b1c1dd2ca5a0773c2c9f0c04eb699e72fb452d
alege <aleggge@gmail.com>
2017-06-18 18:48:31 +0300

    googlemap, input, list added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,8 +2,10 @@ import $ from 'jquery';
 import svg4everybody from 'svg4everybody';
 import 'object-fit-images';
 import menu from '../blocks/menu/menu';
+import mapInit from '../blocks/map/mapInit';
 
 $(() => {
   svg4everybody();
   menu();
+  mapInit();
 });
