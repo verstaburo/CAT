479b1b16db5ab2831fa5f7c223d1c225127d431d
alege <aleggge@gmail.com>
2017-06-27 15:44:26 +0300

    transit commit

--- app/blocks/pagination/pagination.js
+++ app/blocks/pagination/pagination.js
@@ -3,29 +3,43 @@ import 'pagepiling.js';
 const $ = window.$;
 /*eslint-disable*/
 export default () => {
-  if (!document.getElementById('pagepiling')) {
-    return;
+
+  var w = $(window);
+
+  function checkWidth() {
+    if (w.width() > 1024) {
+      loadSlides();
+    }
   }
 
-  $('html, body').css('overflow-y', 'hidden');
+  function loadSlides() {
+    if (!document.getElementById('pagepiling')) {
+      return;
+    }
 
-  $('#pagepiling').pagepiling({
-    anchors: ['main', 'services', 'park-technics', 'contacts'],
-    navigation: false,
-    afterLoad: function(anchorLink, index) {
-      $('.pagination__number').html('0' + index);
-      for (var i = 0; i < index; i++) {
-        $('.pagination__dot').eq(i).addClass('pagination__dot_active');
-      }
-      for (; i < $('.pagination__dot').length; i++) {
-        $('.pagination__dot').eq(i).removeClass('pagination__dot_active');
-      }
-    },
-  });
+    $('html, body').css('overflow-y', 'hidden');
+    $('.main-slide').css('position', 'absolute');
 
-  $.fn.pagepiling.moveSectionDown();
+    $('#pagepiling').pagepiling({
+      anchors: ['main', 'services', 'park-technics', 'contacts'],
+      navigation: false,
+      afterLoad: function(anchorLink, index) {
+        $('.pagination__number').html('0' + index);
+        for (var i = 0; i < index; i++) {
+          $('.pagination__dot').eq(i).addClass('pagination__dot_active');
+        }
+        for (; i < $('.pagination__dot').length; i++) {
+          $('.pagination__dot').eq(i).removeClass('pagination__dot_active');
+        }
+      },
+    });
 
-  setTimeout(function () {
-    $.fn.pagepiling.moveSectionUp();
-  },0);
+    $.fn.pagepiling.moveSectionDown();
+
+    setTimeout(function() {
+      $.fn.pagepiling.moveSectionUp();
+    }, 0);
+  }
+
+  checkWidth();
 };
