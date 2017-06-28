a539316754ec2e02eca808829e73bf062f48cfca
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-06-28 19:37:19 +0300

    load content projects

---
+++ app/static/misc/assets/scripts/ajax/projects.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+$(function () {
+  // Подгрузка первых записей
+  var container = $('.projects'),
+      content = $('.js-projects-container'),
+      spinner = $('<div class="spinner"></div>'),
+      loadBtn = $('.js-projects-show-more');
+
+  $('.js-projects-show-more').click(function(e) {
+    e.preventDefault();
+
+    $.ajax({
+      url: 'projects-ajax.html',
+      type: 'GET',
+      success: function(data) {
+        setTimeout(function () { // emulate server delay
+          container.find('.spinner').remove();
+          content.append(data);
+          loadBtn.prop('disabled', false);
+        }, 1000);
+      },
+      beforeSend: function() {
+        spinner.insertBefore(loadBtn);
+        loadBtn.prop('disabled', true);
+      }
+    });
+  });
+});
