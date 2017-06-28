f33ae86ef3df90c8953b4e9766f1e9761b4bac18
alege <aleggge@gmail.com>
2017-06-28 23:00:24 +0300

    projects: tag hover preview, main: map fix

--- app/pages/main/main.js
+++ app/pages/main/main.js
@@ -1,46 +1,50 @@
 const $ = window.$;
 
-export default () => { // eslint-disable-next-line
+export default () => {
+   /* eslint-disable */
+  //  Открытие списка городов
   $('.left-toggler__item_show').click(function(e) {
     e.preventDefault();
     $('.main__cities').addClass('main__cities_active');
     $('.left-toggler').addClass('left-toggler_active');
   });
-  // eslint-disable-next-line
+  //  Закрытие списка городов
   $('.left-toggler__item_hide').click(function(e) {
     e.preventDefault();
     $('.main__cities').removeClass('main__cities_active');
     $('.left-toggler').removeClass('left-toggler_active');
   });
-  // eslint-disable-next-line
+  // Показать карту
   $('.contact-text__toggler_show').click(function(e) {
     e.preventDefault();
+    // Для декстопа
     if ($(window).width() > 1024) {
       $('.main-slide__map').addClass('main-slide__slide-bg_active');
       $('.main-slide__contact-form').addClass('main-slide__contact-form_disabled');
       $('.contact-text').addClass('contact-text_active');
       $('.main__map .big-heading').css('opacity', '0');
     } else {
+      // Для мобильного
       $('.main-slide__map').addClass('main-slide__slide-bg_active');
       $('.main-slide__contact-text').addClass('main-slide__contact-text_disabled');
       $('.toMap_main').show();
     }
   });
-  // eslint-disable-next-line
+  // Скрыть карту
   $('.contact-text__toggler_hide').click(function(e) {
     e.preventDefault();
     $('.main-slide__map').removeClass('main-slide__slide-bg_active');
     $('.main-slide__contact-form').removeClass('main-slide__contact-form_disabled');
     $('.contact-text').removeClass('contact-text_active');
-    $('.main__map .big-heading').css('opacity', '0');
+    $('.main__map .big-heading').css('opacity', '1');
   });
-  // eslint-disable-next-line
+  // Переход к форме
   $('.toForm').click(function (e) {
     e.preventDefault();
     $('.main-slide__contact-text').addClass('main-slide__contact-text_disabled');
     $('.main-slide__contact-form').removeClass('main-slide__contact-form_disabled');
   });
-  // eslint-disable-next-line
+  // Переход к карте
   $('.toMap').click(function (e) {
     e.preventDefault();
     $('.main-slide__map').removeClass('main-slide__slide-bg_active');
