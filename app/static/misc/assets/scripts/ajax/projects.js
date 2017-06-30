39f7a2fce979bc73c9497fab2a3cc618cbd3b9cb
alege <aleggge@gmail.com>
2017-06-30 19:13:36 +0300

    ajax+popup fix

--- app/static/misc/assets/scripts/ajax/projects.js
+++ app/static/misc/assets/scripts/ajax/projects.js
@@ -15,6 +15,18 @@ $(function () {
           container.find('.spinner').remove();
           content.append(data);
           loadBtn.prop('disabled', false);
+          $('.popup').magnificPopup({
+            type: 'image',
+            closeOnContentClick: true,
+            closeBtnInside: true,
+            fixedContentPos: true,
+            mainClass: 'mfp-fade', // class to remove default margin from left and right side
+            image: {
+              verticalFit: true,
+              hozizontalFit: true,
+            },
+            removalDelay: 300,
+          });
         }, 1000);
       },
       beforeSend: function() {
